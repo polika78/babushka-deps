746cc0d361e3a60a96b77544c331760d0c0657b0
polika78 <polika78@gmail.com>
2013-04-16 17:54:50 +1000

    fix

--- install-hubink.rb
+++ install-hubink.rb
@@ -1,10 +1,54 @@
-dep "install hubink", :version do
+dep 'install hubink' do
+	requires [
+		'bundler'
+		'rubygems'
+		'homebrew'
+		'mysql'
+		'pow'
+		'myadbox'
+		'myadserver'
+		'nodejs'
+		'myadscripts'
+		'indesign'
+		'dropbox'
+		'apache-MAMP'
+	]
+end
+
+dep 'bundler', :version do
 	version.default!('1.3.4')
-	requires "rubygems"
 	met? {
 		in_path? "bundle >= #{version}"
 	}
 	meet {
-		log_shell("Installing Bundler..#{version}", "gem install bundler")
+		log_shell("Installing Bundler..#{version}","gem install bundler")
 	}
+end
+
+dep 'mysql' , :version do
+	
+end
+
+dep 'pow' do
+end
+
+dep 'myadbox' do
+end
+
+dep 'myadserver' do
+end
+
+dep 'nodejs' do
+end
+
+dep 'myadscripts' do
+end
+
+dep 'indesign' do
+end
+
+dep 'dropbox' do
+end
+
+dep 'apache-MAMP' do
 end
