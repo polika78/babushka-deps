9aa5c6aa1e8de3ae23563eb263cdc9a21b27baa7
polika78 <polika78@gmail.com>
2013-04-16 17:00:30 +1000

    fix

--- install-hubink.rb
+++ install-hubink.rb
@@ -2,7 +2,7 @@ dep "install hubink", :version do
 	version.default!('1.3.4')
 	requires "rubygems"
 	met? {
-		in_path "bundle >= #{version}"
+		in_path? "bundle >= #{version}"
 	}
 	meet {
 		log_shell("Installing Bundler..#{version}", "gem install bundler")
