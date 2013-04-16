9a04be6534faa44b312f456f9e70bc07121df255
polika78 <polika78@gmail.com>
2013-04-16 16:53:03 +1000

    add bundler

--- install-hubink.rb
+++ install-hubink.rb
@@ -1,3 +1,10 @@
-dep "install hubink" do
+dep "install hubink", :version do
+	version.default!('1.3.4')
 	require "rubygems"
+	met? {
+		in_path "bundle >= #{version}"
+	}
+	meet {
+		log_shell("Installing Bundler..#{version}", "gem install bundler"}
+	}
 end
