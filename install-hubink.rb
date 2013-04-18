e5d10adb246a97ef64e22db7436068201ae6c367
polika78 <polika78@gmail.com>
2013-04-18 11:15:19 +1000

    fix

--- install-hubink.rb
+++ install-hubink.rb
@@ -49,11 +49,11 @@ dep 'xcode commandline install', :template => 'external' do
 	expects %w[cpp m4 nasm yacc bison] # misc - the preprocessor, assembler, grammar stuff
 	otherwise {
 		log "Install Command Line Tools for Xcode."
-		log "https://s3-ap-southeast-2.amazonaws.com/myadbox-resources/"
+		log "https://s3-ap-southeast-2.amazonaws.com/myadbox-resources/DeveloperToolsCLI.pkg"
 		#source "https://s3-ap-southeast-2.amazonaws.com/myadbox-resources/xcode461_cltools_10_86938245a.dmg"
-		#shell "\"\`curl -O https://s3-ap-southeast-2.amazonaws.com/myadbox-resources/\`\""
-		shell "\"\`curl -O http://cloud.github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.7-v2.pkg\`\""
-		shell "echo volder | sudo -S installer -pkg  GCC-10.7-v2.pkg -target /"
+		shell "\"\`curl -O https://s3-ap-southeast-2.amazonaws.com/myadbox-resources/DeveloperToolsCLI.pkg\`\""
+		#shell "\"\`curl -O http://cloud.github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.7-v2.pkg\`\""
+		shell "sudo installer -pkg  GCC-10.7-v2.pkg -target /"
 	}
 end
 
