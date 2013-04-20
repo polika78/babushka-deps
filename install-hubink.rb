8119f629ede55b1066392028770979bbd156e84e
polika78 <polika78@gmail.com>
2013-04-20 16:51:24 +1000

    fix

--- install-hubink.rb
+++ install-hubink.rb
@@ -134,7 +134,9 @@ dep 'install sequel pro', :template => 'installer'do
 		"/Applications/Sequel Pro.app/".p.exists?
 	}
 	log("Installing Sequel Pro..")
-	source("http://sequel-pro.googlecode.com/files/sequel-pro-1.0.1.dmg")
+	shell "\"\`curl -O http://sequel-pro.googlecode.com/files/sequel-pro-1.0.1.dmg\`\""
+	shell "hdiutil attach sequel-pro-1.0.1.dmg"
+	#source("http://sequel-pro.googlecode.com/files/sequel-pro-1.0.1.dmg")
 	shell('echo volder | sudo -S cp -Rfp /Volumes/Sequel\ Pro\ 1.0.1/ /Applications/')
 end
 
